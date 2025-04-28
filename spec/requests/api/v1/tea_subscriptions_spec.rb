4e9f3925f38e405e5fc5d5a997967bce6e99ef86
Jacob Inman <jrinman6295@gmail.com>
2025-04-28 13:24:59 -0700

    tea sub show spec written

--- spec/requests/api/v1/tea_subscriptions_spec.rb
+++ spec/requests/api/v1/tea_subscriptions_spec.rb
@@ -16,12 +16,31 @@ RSpec.describe "TeaSubscriptions", type: :request do
       json = JSON.parse(response.body, symbolize_names: true)
 
       expect(response).to have_http_status(:ok)
-      expect(json[:data].length).to eq(2)
+      expect(json[:total_subscriptions]).to eq(2)
       expect(json[:data].first[:id]).to eq(@tea_sub1.id.to_s)
+      expect(json[:data].first[:type]).to eq("tea_subscription")
       expect(json[:data].first[:attributes][:frequency]).to eq(@tea_sub1.frequency)
       expect(json[:data].first[:attributes][:status]).to eq(@tea_sub1.status)
       expect(json[:data].first[:attributes][:customer_name]).to eq("Koala Tea Control")
       expect(json[:data].first[:attributes][:tea_type]).to eq(@tea1.name)
+    end
+
+    it "can get a single subscription with more details" do
+      get `/api/v1/tea_subscriptions/#{@tea_sub1.id}`
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to have_http_status(:ok)
+      expect(json[:data][:id]).to eq(@tea_sub1.id.to_s)
+      expect(json[:data][:type]).to eq("tea_subscription")
+      expect(json[:data][:attributes][:frequency]).to eq(@tea_sub1.frequency)
+      expect(json[:data][:attributes][:status]).to eq(@tea_sub1.status)
+      expect(json[:data][:attributes][:customer][:full_name]).to eq("Koala Tea Control")
+      expect(json[:data][:attributes][:customer][:email]).to eq(@customer1.email)
+      expect(json[:data][:attributes][:customer][:address]).to eq(@customer1.address)
+      expect(json[:data][:attributes][:tea][:name]).to eq(@tea1.name)
+      expect(json[:data][:attributes][:tea][:description]).to eq(@tea1.description)
+      expect(json[:data][:attributes][:tea][:brew_time]).to eq(@tea1.brew_time)
+      expect(json[:data][:attributes][:tea][:temperature]).to eq(@tea1.temperature)
     end
   end
 end
