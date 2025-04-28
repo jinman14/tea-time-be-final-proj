a661494fa4d7be6faa946b6ec67c7056d3b331c7
Jacob Inman <jrinman6295@gmail.com>
2025-04-28 10:49:40 -0700

    just before bundle installing factorybot gems

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  
+  resources :tea_subscriptions, only: [:index, :show, :create, :update]
+  end
 end
---
+++ spec/requests/api/v1/tea_subscriptions_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper" 
+
+RSpec.describe "TeaSubscriptions", type: :request do
+  describe "GET" do
+    it "returns a list of all tea_subscriptions" do
+      
+    end
+  end
+end
