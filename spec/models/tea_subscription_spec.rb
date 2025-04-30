067362bd45c971db85cfda95da62b12e74fdb18e
Jacob Inman <jrinman6295@gmail.com>
2025-04-30 08:29:42 -0700

    added spec for tsvector on status in models. coverage at 100%

--- spec/models/tea_subscription_spec.rb
+++ spec/models/tea_subscription_spec.rb
@@ -6,4 +6,25 @@ RSpec.describe TeaSubscription, type: :model do
     it { should belong_to :tea }
     it { should define_enum_for(:status).with_values(active: 0, inactive: 1) }
   end
+
+  describe "tsvector scope" do
+    before(:each) do
+      @customer1 = create(:customer, first_name: "Koala", last_name: "Tea Control" )
+      @customer2 = create(:customer, first_name: "Cutie", last_name: "Patootie")
+      @tea1 = create(:tea, name: "Fireside Vanilla")
+      @tea2 = create(:tea, name: "Tension Tamer")
+  
+      @tea_sub1 = create(:tea_subscription, customer: @customer1, tea: @tea1, status: "active", frequency: "monthly")
+      @tea_sub2 = create(:tea_subscription, customer: @customer2, tea: @tea2, status: "active", frequency: "daily")
+      @tea_sub3 = create(:tea_subscription, customer: @customer1, tea: @tea2, status: "inactive", frequency: "daily")
+    end
+
+    it "returns a grouping of matching statuses" do
+      expect(TeaSubscription.search("active")).to include(@tea_sub1, @tea_sub2)
+    end
+
+    it "returns a grouping of matching frequencies" do
+      expect(TeaSubscription.search("daily")).to include(@tea_sub2, @tea_sub3)
+    end
+  end
 end
